5efdcdd6854377b6c43230d9dbcc211add585bbe
Wayne Chen (陳灝文) <waynechen.cybersoft.com.tw>
2024-07-04 17:49:50 +0800

    [修改]修改namespace

--- ListedCompany/ListedCompany/Controllers/ListedCompanyController.cs
+++ ListedCompany/ListedCompany/Controllers/ListedCompanyController.cs
@@ -1,43 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace ListedCompany.Controllers
+namespace ListedCompany.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ListedCompanyController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ListedCompanyController : ControllerBase
+    
+    [HttpGet]
+    public IEnumerable<string> Get()
     {
-        // GET: api/<ListedCompanyController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        return new string[] { "value1", "value2" };
+    }
 
-        // GET api/<ListedCompanyController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+    
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
 
-        // POST api/<ListedCompanyController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+    
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
 
-        // PUT api/<ListedCompanyController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+    
+    [HttpPatch("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
 
-        // DELETE api/<ListedCompanyController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+    
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
     }
 }
